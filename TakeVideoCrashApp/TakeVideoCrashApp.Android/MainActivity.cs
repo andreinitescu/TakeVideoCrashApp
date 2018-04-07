6eaae151a9f1cccffda761a73a39aa8e01b9e2d5
Andrei N <nitescua@yahoo.com>
2018-04-07 13:57:44 +0300

    Commit

---
+++ TakeVideoCrashApp/TakeVideoCrashApp.Android/MainActivity.cs
@@ -0,0 +1,41 @@
+﻿using Android.App;
+using Android.Content.PM;
+using Android.OS;
+using Plugin.Permissions;
+using System;
+using TakeVideoCrashApp;
+
+namespace TakeVideoCrashApp.Droid
+{
+    [Activity(Label = "TakeVideoCrashApp", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(bundle);
+
+            Plugin.CurrentActivity.CrossCurrentActivity.Current.Activity = this;
+
+            global::Xamarin.Forms.Forms.Init(this, bundle);
+
+            try
+            {
+                LoadApplication(new App());
+            }
+            catch(Exception ex)
+            {
+
+            }
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, Android.Content.PM.Permission[] grantResults)
+        {
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+            PermissionsImplementation.Current.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
+
---
+++ TakeVideoCrashApp/TakeVideoCrashApp/MainPage.xaml.cs
@@ -0,0 +1,72 @@
+﻿using Plugin.Media;
+using Plugin.Media.Abstractions;
+using Plugin.Permissions;
+using Plugin.Permissions.Abstractions;
+using System;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace TakeVideoCrashApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            Btn.Clicked += Btn_Clicked;
+        }
+
+        private async void Btn_Clicked(object sender, EventArgs e)
+        {
+            if (!await EnsurePermissions())
+            {
+                await DisplayAlert("Permissions Denied", "Unable to take photos.", "OK");
+                return;
+            }
+
+            MakeNewVideo();
+        }
+
+        async Task<bool> EnsurePermissions()
+        {
+            var cameraStatus = await CrossPermissions.Current.CheckPermissionStatusAsync(Permission.Camera);
+            var storageStatus = await CrossPermissions.Current.CheckPermissionStatusAsync(Permission.Storage);
+
+            if (cameraStatus != PermissionStatus.Granted || storageStatus != PermissionStatus.Granted)
+            {
+                var results = await CrossPermissions.Current.RequestPermissionsAsync(new[] { Permission.Camera, Permission.Storage });
+                cameraStatus = results[Permission.Camera];
+                storageStatus = results[Permission.Storage];
+            }
+
+            return cameraStatus == PermissionStatus.Granted && storageStatus == PermissionStatus.Granted;
+        }
+
+        async void MakeNewVideo()
+        {
+            IMedia media = CrossMedia.Current;
+
+            await media.Initialize();
+            if (media.IsTakeVideoSupported)
+            {
+                MediaFile result = await media.TakePhotoAsync(new StoreVideoOptions()
+                //MediaFile result = await media.TakeVideoAsync(new StoreVideoOptions()
+                {
+                    Quality = VideoQuality.Medium,
+                    DefaultCamera = CameraDevice.Rear,
+                    SaveToAlbum = true
+                });
+
+                if (result != null)
+                {
+                    await DisplayAlert("Perfect", "OK", "Cancel");
+                }
+                else
+                {
+                    await DisplayAlert("Not good", "OK", "Cancel");
+                }
+            }
+        }
+    }
+}
